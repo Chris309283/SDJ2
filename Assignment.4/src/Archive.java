026d3532c3c62159998975ef3e15e8006f55ac66
Chris309283 <80517220+Chris309283@users.noreply.github.com>
2021-11-24 00:06:37 +0100

    start assignment 4

---
+++ Assignment.4/src/Archive.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+
+public class Archive
+{
+  private File logFile;
+  private static Archive archive;
+  private static final Lock lock = new ReentrantLock();
+
+  public Archive()
+  {
+    logFile = new File("LogFile.txt");
+  }
+
+  public static Archive getInstance()
+  {
+    if (archive == null)
+    {
+      synchronized (lock)
+      {
+        if (archive == null)
+        {
+          archive = new Archive();
+        }
+      }
+    }
+    return archive;
+  }
+
+  public void log(String txt)
+  {
+    try
+    {
+      System.out.println("Log --> " + txt);
+      Writer out = new BufferedWriter(new FileWriter(logFile, true));
+      out.append(txt);
+      out.flush();
+      out.close();
+    }
+    catch (IOException e)
+    {
+      e.printStackTrace();
+    }
+  }
+}
